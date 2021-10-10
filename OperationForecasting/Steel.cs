1e38bce5a04894f422f4d73753c7ea1b05b73a64
CoerShada <mmed911@mail.ru>
2021-10-10 23:22:05 +0700

    ver10102021

--- OperationForecasting/Steel.cs
+++ OperationForecasting/Steel.cs
@@ -8,14 +8,38 @@ namespace OperationForecasting
 {
     abstract class Steel
     {
-        public abstract double AmplitudeOfInternalStressFields();
 
-        public abstract double ShearStresses();
+        private double _CoefRatio;
 
-        public abstract double DeformationIndicator1();
+        public Steel(double CoefRatio)
+        {
+            _CoefRatio = CoefRatio;
+        }
 
-        public abstract double DeformationIndicator2();
+        public abstract double GetAmplitudeOfInternalStressFields();
 
+        public abstract double GetShearStresses();
+
+        public abstract double GetDeformationIndicator1();
+
+        public abstract double GetDeformationIndicator2();
+
+        public abstract double GetRatioOfYieldStrengthToElongation();
+
+        public double GetCoefStructuralMechanical()
+        {
+            double tl = GetAmplitudeOfInternalStressFields();
+            double td = GetShearStresses();
+            double a1 = GetDeformationIndicator1();
+            double a2 = GetDeformationIndicator2();
+
+            return (tl / td) * ((a2 - a1) / (a1 + a2) * Math.Log(GetRatioOfYieldStrengthToElongation());
+        }
+
+        /*public sealed double RemainingRunningTime()
+        {
+            return 
+        }*/
 
     }
 }
