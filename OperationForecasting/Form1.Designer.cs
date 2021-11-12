e32ed2f9eab297ef8473e51beb00ebe33dac7123
Kirill <kirill.nesterenko11@mail.ru>
2021-11-12 21:59:05 +0700

    Сделано заполнение комбобокса марки и добавлено событие на кнопку рассчитать

--- OperationForecasting/Form1.Designer.cs
+++ OperationForecasting/Form1.Designer.cs
@@ -33,9 +33,8 @@ namespace OperationForecasting
             this.textBoxR = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBoxDatchik = new System.Windows.Forms.ComboBox();
+            this.comboBoxMarka = new System.Windows.Forms.ComboBox();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
@@ -44,227 +43,221 @@ namespace OperationForecasting
             this.label7 = new System.Windows.Forms.Label();
             this.label8 = new System.Windows.Forms.Label();
             this.label9 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBoxK = new System.Windows.Forms.TextBox();
+            this.textBoxA = new System.Windows.Forms.TextBox();
+            this.textBoxV = new System.Windows.Forms.TextBox();
+            this.textBoxMINI = new System.Windows.Forms.TextBox();
+            this.textBoxSrok = new System.Windows.Forms.TextBox();
             this.label10 = new System.Windows.Forms.Label();
             this.labelRRT = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(268, 195);
+            this.button1.Location = new System.Drawing.Point(357, 240);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(125, 30);
+            this.button1.Size = new System.Drawing.Size(167, 37);
             this.button1.TabIndex = 0;
             this.button1.Text = "Рассчитать";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // textBoxR
             // 
-            this.textBoxR.Location = new System.Drawing.Point(68, 69);
+            this.textBoxR.Location = new System.Drawing.Point(91, 85);
+            this.textBoxR.Margin = new System.Windows.Forms.Padding(4);
             this.textBoxR.Name = "textBoxR";
-            this.textBoxR.Size = new System.Drawing.Size(100, 20);
+            this.textBoxR.Size = new System.Drawing.Size(132, 22);
             this.textBoxR.TabIndex = 1;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(20, 72);
+            this.label1.Location = new System.Drawing.Point(27, 89);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(36, 13);
+            this.label1.Size = new System.Drawing.Size(47, 17);
             this.label1.TabIndex = 2;
             this.label1.Text = "R (нс)";
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(399, 196);
+            this.button2.BackColor = System.Drawing.Color.Red;
+            this.button2.Location = new System.Drawing.Point(532, 241);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(125, 30);
+            this.button2.Size = new System.Drawing.Size(167, 37);
             this.button2.TabIndex = 3;
             this.button2.Text = "Справка";
-            this.button2.UseVisualStyleBackColor = true;
+            this.button2.UseVisualStyleBackColor = false;
             // 
-            // button3
+            // comboBoxDatchik
             // 
-            this.button3.Location = new System.Drawing.Point(374, 304);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(150, 35);
-            this.button3.TabIndex = 4;
-            this.button3.Text = "Расчитать остаточное время эксплуатации";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.comboBoxDatchik.BackColor = System.Drawing.SystemColors.MenuText;
+            this.comboBoxDatchik.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxDatchik.FormattingEnabled = true;
+            this.comboBoxDatchik.Location = new System.Drawing.Point(484, 81);
+            this.comboBoxDatchik.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.comboBoxDatchik.Name = "comboBoxDatchik";
+            this.comboBoxDatchik.Size = new System.Drawing.Size(132, 24);
+            this.comboBoxDatchik.TabIndex = 5;
             // 
-            // comboBox1
+            // comboBoxMarka
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(363, 66);
-            this.comboBox1.Margin = new System.Windows.Forms.Padding(2);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(100, 21);
-            this.comboBox1.TabIndex = 5;
-            // 
-            // comboBox2
-            // 
-            this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(363, 107);
-            this.comboBox2.Margin = new System.Windows.Forms.Padding(2);
-            this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(100, 21);
-            this.comboBox2.TabIndex = 6;
-            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            this.comboBoxMarka.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxMarka.FormattingEnabled = true;
+            this.comboBoxMarka.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.comboBoxMarka.Location = new System.Drawing.Point(484, 132);
+            this.comboBoxMarka.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.comboBoxMarka.Name = "comboBoxMarka";
+            this.comboBoxMarka.Size = new System.Drawing.Size(132, 24);
+            this.comboBoxMarka.TabIndex = 6;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(13, 106);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Location = new System.Drawing.Point(17, 130);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.Size = new System.Drawing.Size(55, 17);
             this.label2.TabIndex = 7;
             this.label2.Text = "Кзат = ";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(30, 140);
-            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Location = new System.Drawing.Point(40, 172);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(26, 13);
+            this.label3.Size = new System.Drawing.Size(33, 17);
             this.label3.TabIndex = 8;
             this.label3.Text = "A = ";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(30, 174);
-            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Location = new System.Drawing.Point(40, 214);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(26, 13);
+            this.label4.Size = new System.Drawing.Size(33, 17);
             this.label4.TabIndex = 9;
             this.label4.Text = "V = ";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(15, 205);
-            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Location = new System.Drawing.Point(20, 252);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(42, 13);
+            this.label5.Size = new System.Drawing.Size(51, 17);
             this.label5.TabIndex = 10;
             this.label5.Text = "MINI = ";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(265, 144);
-            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Location = new System.Drawing.Point(353, 177);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(77, 26);
+            this.label6.Size = new System.Drawing.Size(99, 34);
             this.label6.TabIndex = 11;
             this.label6.Text = "Срок \r\nэксплуатации";
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(265, 69);
-            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label7.Location = new System.Drawing.Point(353, 85);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(44, 13);
+            this.label7.Size = new System.Drawing.Size(57, 17);
             this.label7.TabIndex = 12;
             this.label7.Text = "Датчик";
             // 
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(265, 110);
-            this.label8.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label8.Location = new System.Drawing.Point(353, 135);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(40, 13);
+            this.label8.Size = new System.Drawing.Size(50, 17);
             this.label8.TabIndex = 13;
             this.label8.Text = "Марка";
             // 
             // label9
             // 
             this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(14, 254);
-            this.label9.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label9.Location = new System.Drawing.Point(19, 313);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(43, 13);
+            this.label9.Size = new System.Drawing.Size(54, 17);
             this.label9.TabIndex = 14;
             this.label9.Text = "Кс-м = ";
             // 
-            // textBox1
+            // textBoxK
             // 
-            this.textBox1.Location = new System.Drawing.Point(68, 103);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 15;
+            this.textBoxK.Location = new System.Drawing.Point(91, 127);
+            this.textBoxK.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBoxK.Name = "textBoxK";
+            this.textBoxK.Size = new System.Drawing.Size(132, 22);
+            this.textBoxK.TabIndex = 15;
             // 
-            // textBox2
+            // textBoxA
             // 
-            this.textBox2.Location = new System.Drawing.Point(68, 137);
-            this.textBox2.Margin = new System.Windows.Forms.Padding(2);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
-            this.textBox2.TabIndex = 16;
+            this.textBoxA.BackColor = System.Drawing.Color.Red;
+            this.textBoxA.Location = new System.Drawing.Point(91, 169);
+            this.textBoxA.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBoxA.Name = "textBoxA";
+            this.textBoxA.Size = new System.Drawing.Size(132, 22);
+            this.textBoxA.TabIndex = 16;
             // 
-            // textBox3
+            // textBoxV
             // 
-            this.textBox3.Location = new System.Drawing.Point(68, 171);
-            this.textBox3.Margin = new System.Windows.Forms.Padding(2);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(100, 20);
-            this.textBox3.TabIndex = 17;
+            this.textBoxV.BackColor = System.Drawing.Color.Red;
+            this.textBoxV.Location = new System.Drawing.Point(91, 210);
+            this.textBoxV.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBoxV.Name = "textBoxV";
+            this.textBoxV.Size = new System.Drawing.Size(132, 22);
+            this.textBoxV.TabIndex = 17;
             // 
-            // textBox4
+            // textBoxMINI
             // 
-            this.textBox4.Location = new System.Drawing.Point(68, 205);
-            this.textBox4.Margin = new System.Windows.Forms.Padding(2);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(100, 20);
-            this.textBox4.TabIndex = 18;
+            this.textBoxMINI.BackColor = System.Drawing.Color.Red;
+            this.textBoxMINI.Location = new System.Drawing.Point(91, 252);
+            this.textBoxMINI.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBoxMINI.Name = "textBoxMINI";
+            this.textBoxMINI.Size = new System.Drawing.Size(132, 22);
+            this.textBoxMINI.TabIndex = 18;
             // 
-            // textBox5
+            // textBoxSrok
             // 
-            this.textBox5.Location = new System.Drawing.Point(363, 150);
-            this.textBox5.Margin = new System.Windows.Forms.Padding(2);
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(100, 20);
-            this.textBox5.TabIndex = 19;
+            this.textBoxSrok.Location = new System.Drawing.Point(484, 185);
+            this.textBoxSrok.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBoxSrok.Name = "textBoxSrok";
+            this.textBoxSrok.Size = new System.Drawing.Size(132, 22);
+            this.textBoxSrok.TabIndex = 19;
             // 
             // label10
             // 
             this.label10.AutoSize = true;
-            this.label10.Location = new System.Drawing.Point(189, 7);
-            this.label10.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label10.Location = new System.Drawing.Point(252, 9);
             this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(183, 13);
+            this.label10.Size = new System.Drawing.Size(240, 17);
             this.label10.TabIndex = 20;
             this.label10.Text = "Структурно-механическй критерий";
             // 
             // labelRRT
             // 
-            this.labelRRT.Location = new System.Drawing.Point(65, 254);
+            this.labelRRT.Location = new System.Drawing.Point(87, 313);
+            this.labelRRT.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.labelRRT.Name = "labelRRT";
-            this.labelRRT.Size = new System.Drawing.Size(459, 38);
+            this.labelRRT.Size = new System.Drawing.Size(612, 47);
             this.labelRRT.TabIndex = 21;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(561, 351);
+            this.ClientSize = new System.Drawing.Size(748, 432);
             this.Controls.Add(this.labelRRT);
             this.Controls.Add(this.label10);
-            this.Controls.Add(this.textBox5);
-            this.Controls.Add(this.textBox4);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.textBoxSrok);
+            this.Controls.Add(this.textBoxMINI);
+            this.Controls.Add(this.textBoxV);
+            this.Controls.Add(this.textBoxA);
+            this.Controls.Add(this.textBoxK);
             this.Controls.Add(this.label9);
             this.Controls.Add(this.label8);
             this.Controls.Add(this.label7);
@@ -273,13 +266,13 @@ namespace OperationForecasting
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.comboBox2);
-            this.Controls.Add(this.comboBox1);
-            this.Controls.Add(this.button3);
+            this.Controls.Add(this.comboBoxMarka);
+            this.Controls.Add(this.comboBoxDatchik);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBoxR);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -293,9 +286,8 @@ namespace OperationForecasting
         private System.Windows.Forms.TextBox textBoxR;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBoxDatchik;
+        private System.Windows.Forms.ComboBox comboBoxMarka;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
@@ -304,11 +296,11 @@ namespace OperationForecasting
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Label label8;
         private System.Windows.Forms.Label label9;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBoxK;
+        private System.Windows.Forms.TextBox textBoxA;
+        private System.Windows.Forms.TextBox textBoxV;
+        private System.Windows.Forms.TextBox textBoxMINI;
+        private System.Windows.Forms.TextBox textBoxSrok;
         private System.Windows.Forms.Label label10;
         private System.Windows.Forms.Label labelRRT;
     }
--- OperationForecasting/Form1.cs
+++ OperationForecasting/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -27,8 +28,28 @@ namespace OperationForecasting
 
         private void InitComponentsContent()
         {
-
+            Steel[] steels = handler.GetSteels();
+            string[] steelsNames = new string[steels.Length];
+            for (int i = 0; i<steels.Length; i++)
+            {
+                steelsNames[i] = steels[i].GetSteelName();
+            }
+            this.comboBoxMarka.Items.AddRange(steelsNames);
+            this.comboBoxMarka.SelectedIndex = 0;
         }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int indexSteel = this.comboBoxMarka.SelectedIndex;
+            Steel steel = this.handler.GetSteels()[indexSteel];
+
+            string strCurrentTime = this.textBoxR.Text.Replace(',', '.').Trim();
+            double currentTime = double.Parse(strCurrentTime, new NumberFormatInfo { NumberDecimalSeparator = "." });
+            string strR = this.textBoxR.Text.Replace(',', '.').Trim();
+            double R = double.Parse(strR, new NumberFormatInfo { NumberDecimalSeparator = "." });
+            string strK = this.textBoxK.Text.Replace(',', '.').Trim();
+            double K = double.Parse(strK, new NumberFormatInfo { NumberDecimalSeparator = "." });
+            this.labelRRT.Text = this.handler.GetOutMessage(steel, currentTime, R, K);
+        }
     }
 }
